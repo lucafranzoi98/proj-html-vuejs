026652589cab300cf6b25aed0f600392f64dc840
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-10-03 10:38:22 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import { state } from "./state.js";
+
+export default{
+   name: "App",
+   components: {
+
+   },
+   data (){
+      return {
+         state
+      }
+   }
+}
+</script>
+
+<template>
+
+   <div class="text-warning">test</div>
+
+</template>
+
+<style >
+
+</style>
